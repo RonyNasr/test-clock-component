fea2daf8b133547a235aa17853cc733711a1969d
Rony Nasr <nasr.rony@gmail.com>
2017-10-03 04:52:43 -0700

    updated component to display welcome message

--- src/components/test-clock-component.vue
+++ src/components/test-clock-component.vue
@@ -1,10 +1,19 @@
 <template>
-  <div>{{ display }}</div>
+  <div>
+    <h1>{{ message }}</h1>
+    <h2>{{ display }}</h2>
+  </div>
 </template>
 <script>
   import moment from 'moment'
 
   export default {
+    props: {
+      message: {
+        type: String,
+        default: 'Hello!',
+      },
+    },
     data () {
       return {
         time: Date.now(),
